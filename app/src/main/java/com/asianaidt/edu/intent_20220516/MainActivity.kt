789c683dc0adb855e50f5f9980eee592fca4dcc7
Jin <lycanjin@gmail.com>
2022-05-16 16:52:19 +0900

    Jin 1. Intent 복귀 기능 실습

--- app/src/main/java/com/asianaidt/edu/intent_20220516/MainActivity.kt
+++ app/src/main/java/com/asianaidt/edu/intent_20220516/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.asianaidt.edu.intent_20220516
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_move.setOnClickListener {
+
+            // 다른 화면으로 넘어가기
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
     }
 }
--- app/src/main/java/com/asianaidt/edu/intent_20220516/OtherActivity.kt
+++ app/src/main/java/com/asianaidt/edu/intent_20220516/OtherActivity.kt
@@ -2,10 +2,18 @@ package com.asianaidt.edu.intent_20220516
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+
+        btn_back.setOnClickListener {
+
+            finish()
+
+        }
     }
 }
